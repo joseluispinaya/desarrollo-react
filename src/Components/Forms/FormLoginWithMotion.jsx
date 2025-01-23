ad3efcf66dbc0838db0a6e6092df067cd0cdf8ef
zerobyte <joseluisdelta1@gmail.com>
2025-01-23 03:38:54 -0400

    add modal

--- src/Components/Forms/FormLoginWithMotion.jsx
+++ src/Components/Forms/FormLoginWithMotion.jsx
@@ -1,7 +1,7 @@
-//import { useState } from "react";
+import { useState } from "react";
 import { motion } from "motion/react"
 import useForm from "../Hooks/useForm.js";
-//import ModalInfo from "../../Components/Modals/ModalInfo.jsx";
+import ModalInfo from "../../Components/Modals/ModalInfo.jsx";
 
 // eslint-disable-next-line react/prop-types
 const FormWithMotionAndHook = ({titleForm}) => {
@@ -9,11 +9,15 @@ const FormWithMotionAndHook = ({titleForm}) => {
         username: '',
         email: ''
     });
-    //const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
     const handleSubmit = (e) => {
         e.preventDefault();
-        //setShowModal(true);
+        setShowModal(true);
         console.log('datos del formulario', formData);
+    };
+
+    const onCloseModalInfo = () => {
+        setShowModal(false);
     };
 
     return (
@@ -23,6 +27,12 @@ const FormWithMotionAndHook = ({titleForm}) => {
             exit={{opacity: 0}}
             transition={{duration: 0.5}}
         >
+            <ModalInfo
+                visible={showModal}
+                message="Formulario enviado!!!"
+                onClose={onCloseModalInfo}
+            />
+
             <form onSubmit={handleSubmit}>
                 <motion.div
                     initial={{x: -100}}
